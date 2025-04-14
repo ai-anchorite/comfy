7bc3eacebc15f40fc8c72819fae6f7660ce9f338
Anchorite <ai_anchorite@proton.me>
2025-04-14 17:27:20 +1200

    Update install.js

--- install.js
+++ install.js
@@ -48,6 +48,47 @@ module.exports = {
           // xformers: true   // uncomment this line if your project requires xformers
         }
       }
-    }
+    },
+    {
+      method: "fs.link",
+      params: {
+        drive: {
+          "checkpoints": "app/models/checkpoints",
+          "clip": "app/models/clip",
+          "clip_vision": "app/models/clip_vision",
+          "configs": "app/models/configs",
+          "controlnet": "app/models/controlnet",
+          "diffusers": "app/models/diffusers",
+          "diffusion_models": "app/models/diffusion_models",
+          "embeddings": "app/models/embeddings",
+          "gligen": "app/models/gligen",          
+          "hypernetworks": "app/models/hypernetworks",          
+          "ipadapter": "app/models/ipadapter",
+          "loras": "app/models/loras",
+          "photomaker": "app/models/photomaker",          
+          "style_models": "app/models/style_models",          
+          "text_encoders": "/models/text_encoders",
+          "unet": "app/models/unet",          
+          "upscale_models": "app/models/upscale_models",
+          "vae": "app/models/vae",
+          "vae_approx": "app/models/VAE-approx",
+        },
+        "peers": [
+          "https://github.com/cocktailpeanut/fluxgym.git",
+          "https://github.com/cocktailpeanutlabs/automatic1111.git",
+          "https://github.com/cocktailpeanutlabs/fooocus.git",
+          "https://github.com/cocktailpeanutlabs/comfyui.git",
+          "https://github.com/pinokiofactory/stable-diffusion-webui-forge.git",
+        ]
+      }
+    },
+    {
+      "method": "fs.link",
+      "params": {
+        "drive": {
+          "output": "app/output"
+        }
+      }
+    },
   ]
 }
